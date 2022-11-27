6a1fb993e687e617fa0f4afdc2e8d2602c4c0e1e
Artem <artem.mikhaylov5593@yandex.ru>
2022-11-27 20:48:34 +0300

    created healthcheck for api

--- todolist/urls.py
+++ todolist/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from todolist.views import health_check
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('ping/', health_check, name='health_check'),
 ]
