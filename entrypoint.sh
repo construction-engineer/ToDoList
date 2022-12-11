4d31eca9ca1096e2c26a013ef1568daa8843176a
Artem <artem.mikhaylov5593@yandex.ru>
2022-12-11 16:35:05 +0300

    fixed migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
-python manage.py migrate --check
+python3 manage.py migrate --check
 status=#?
 if [[ $status != 0 ]]; then
-  python manage.py migrate
+  python3 manage.py migrate
 fi
 exec "$@"
