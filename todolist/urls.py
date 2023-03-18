d44042d92073c4cfcf449571c7002a8a9545ff80
Artem <artem.mikhaylov5593@yandex.ru>
2023-03-18 17:53:31 +0300

    Complete step 1.1
    Add to Core file `urls.py` and include this url pattern in
    todolist/urls.py.

--- todolist/urls.py
+++ todolist/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from todolist.views import health_check
@@ -24,4 +24,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('ping/', health_check, name='health_check'),
+    path('core/', include('core.urls')),
 ]
